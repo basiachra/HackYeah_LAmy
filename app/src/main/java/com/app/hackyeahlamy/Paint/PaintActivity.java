96b93e331dbde35e7c6589f75b37c387a76152d9
basiachra <basia.chra@gmail.com>
2018-11-24 16:39:44 +0100

    Paint

---
+++ app/src/main/java/com/app/hackyeahlamy/Paint/PaintActivity.java
@@ -0,0 +1,30 @@
+package com.app.hackyeahlamy.Paint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import com.app.hackyeahlamy.R;
+
+public class PaintActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
